053c43a4d2cc9b97e572c4a1e8d46c60f55032c2
Lucas Theis <lucas@theis.io>
2015-01-23 18:15:09 +0100

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from pgf import __version__
+from setuptools import setup
+
+setup(
+	name='pgf',
+	version=__version__,
+	author='Lucas Theis',
+	author_email='lucas@theis.io',
+	packages=['pgf'],
+	license='MIT',
+)
